be58d3c6d1724632b406fcb974cca4c413fb50dd
Lee Kiri <leekiritempleton@gmail.com>
2021-03-23 10:20:01 +1100

    mongoose schema created

--- models/transaction.js
+++ models/transaction.js
@@ -1,1 +1,49 @@
+const mongoose = require("mongoose");
 
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  excercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter a type of exercise",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter a name for this exercise",
+      },
+      duration: {
+        type: Number,
+        trim: true,
+        required: "Enter a time in minutes",
+      },
+      weight: {
+        type: Number,
+        trim: true,
+      },
+      reps: {
+        type: Number,
+        trim: true,
+      },
+      sets: {
+        type: Number,
+        trim: true,
+      },
+      distance: {
+        type: Number,
+        trim: true,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
