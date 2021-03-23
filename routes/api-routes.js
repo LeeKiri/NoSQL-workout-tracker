13cb2e889fd229c2919dbec2e14eab56636e839f
Lee Kiri <leekiritempleton@gmail.com>
2021-03-23 15:10:52 +1100

    html routes complete

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,41 +1,51 @@
-const app = require("express");
+const app = require("express").Router();
 const Workout = require("../models/workout.js");
+const mongoose = require("mongoose");
 
 app.get("/api/workouts", (req, res) => {
-    Workout.find({}).then(dbWorkout => {
-        res.json(dbWorkout);
+  Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
     })
-    .catch(err => {
-        res.status(400).json(err);
-    })
-})
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
-app.put("/api/workouts/id", (req, res) => {
-    Workout.insert(body)
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    }) 
-    .catch(err => {
-        res.status(400).json(err);
+app.put("/api/workouts/:id", (req, res) => {
+  console.log("id", req.params.id);
+  Workout.findByIdAndUpdate(
+    { _id: req.params.id },
+    {
+      $push: { exercises: req.body },
+    }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
     })
-})
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
 app.post("/api/workouts", (req, res) => {
-    Workout.create(body)
-    .then(dbWorkout => {
-        res.json(dbWorkout);
+  Workout.create(req.body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
     })
-    .catch(err => {
-        res.status(400).json(err);
-    })
-})
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
 app.get("/api/workouts/range", (req, res) => {
-    Workout.find({})
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    }) 
-    .catch(err => {
-        res.status(400).json(err);
+  Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
     })
-})
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = app;
